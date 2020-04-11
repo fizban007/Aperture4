84c3eab877db47759ff570ef34264411b27888f8
fizban007 <fizban007@gmail.com>
2020-04-11 10:18:32 -0400

    added basic grid structure class

---
+++ src/core/grid.hpp
@@ -0,0 +1,142 @@
+#ifndef __CORE_GRID_H_
+#define __CORE_GRID_H_
+
+#include "cuda_control.h"
+#include "typedefs_and_constants.h"
+#include "utils/vec.hpp"
+#include "utils/index.hpp"
+#include <type_traits>
+
+namespace Aperture {
+
+template <int Dim>
+struct Grid {
+  uint32_t dims[Dim];  //!< Dimensions of the grid of each direction
+  int guard[Dim];      //!< Number of guard cells at either end of each
+                       //!< direction
+  int skirt[Dim];
+
+  Scalar delta[Dim];  //!< Grid spacing on each direction (spacing in
+                      //!< coordinate space)
+  Scalar inv_delta[Dim];
+  Scalar lower[Dim];  //!< Lower limit of the grid on each direction
+  Scalar sizes[Dim];  //!< Size of the grid in coordinate space
+
+  // int tileSize[Dim];
+  int offset[Dim];
+
+  HOST_DEVICE Grid() = default;
+
+  ///  Reduced dimension in one direction.
+  ///
+  ///  Reduced dimension means the total size of the grid minus the
+  ///  guard cells in both ends. This function is only defined for N >=
+  ///  0 and N < Dim.
+  template <int N>
+      HD_INLINE std::enable_if_t <
+      N<Dim, uint32_t> reduced_dim() const {
+    return (dims[N] - 2 * skirt[N]);
+  }
+
+  uint32_t reduced_dim(int i) const { return (dims[i] - 2 * skirt[i]); }
+  ///  Coordinate of a point inside cell n in dimension N.
+  ///
+  ///  This function applies to field points. Stagger = false means
+  ///  field is defined at cell center, while stagger = true means field
+  ///  defined at cell boundary at the end.
+  ///
+  ///  This calculation is assuming boundary located at the interface
+  ///  between guard cells and physical cells. The function is only
+  ///  defined for N >= 0 and N < Dim.
+  template <int N>
+      HD_INLINE std::enable_if_t <
+      N<Dim, Scalar> pos(int n, bool stagger) const {
+    return pos<N>(n, (int)stagger);
+  }
+
+  ///  Coordinate of a point inside cell n in dimension i.
+  ///
+  ///  This function applies to field points. Stagger = 0 means field is
+  ///  defined at cell center, while stagger = 1 means field defined at
+  ///  cell boundary at the end.
+  ///
+  ///  This calculation is assuming boundary located at the interface
+  ///  between guard cells and physical cells. The function is only
+  ///  defined for N >= 0 and N < DIM.
+  template <int N>
+      HD_INLINE std::enable_if_t <
+      N<Dim, Scalar> pos(int n, int stagger) const {
+    return pos<N>(n, (Scalar)(stagger * 0.5 + 0.5));
+  }
+
+  ///  Coordinate of a point inside cell n in dimension i.
+  ///
+  ///  This function applies to particles. pos_in_cell is the relative
+  ///  position of the particle in the cell and varies from 0.0 to 1.0.
+  ///
+  ///  This calculation is assuming boundary located at the interface
+  ///  between guard cells and physical cells. The function is only
+  ///  defined for N >= 0 and N < Dim. When N >= Dim, it simply returns
+  ///  pos_in_cell
+  template <int N>
+      HD_INLINE std::enable_if_t <
+      N<Dim, Scalar> pos(int n, Scalar pos_in_cell) const {
+    return (lower[N] + delta[N] * (n - skirt[N] + pos_in_cell));
+  }
+
+  template <int N>
+  HD_INLINE std::enable_if_t<N >= Dim, Scalar> pos(
+      int n, Scalar pos_in_cell) const {
+    return pos_in_cell;
+  }
+
+  ///  Find the zone the cell belongs to (for communication purposes)
+  HD_INLINE int find_zone(const index_t<Dim>& pos) const {
+    auto z = vec_t<uint32_t, Dim>{};
+    auto ext = extent_t<Dim>{};
+#pragma unroll
+    for (int i = 0; i < Dim; i++) {
+      z[i] = (pos[i] >= skirt[i]) + (pos[i] >= (dims[i] - skirt[i]));
+      ext[i] = 3;
+    }
+
+    // For now we always use col major for zone index
+    idx_col_major_t<Dim> idx(z, ext);
+    return idx.linear;
+  }
+
+  HD_INLINE bool is_in_bound(const index_t<Dim>& idx) const {
+#pragma unroll
+    for (int i = 0; i < Dim; i++) {
+      if (idx[i] < skirt[i] || idx[i] >= dims[i] - skirt[i])
+        return false;
+    }
+    return true;
+  }
+
+  template <typename... Args>
+  HD_INLINE bool is_in_bound(Args... args) const {
+    return is_in_bound(index(args...));
+  }
+
+  HD_INLINE extent_t<Dim> extent() const {
+    extent_t<Dim> result;
+#pragma unroll
+    for (int i = 0; i < Dim; i++)
+      result[i] = dims[i];
+    return result;
+  }
+
+  HD_INLINE extent_t<Dim> extent_less() const {
+    extent_t<Dim> result;
+#pragma unroll
+    for (int i = 0; i < Dim; i++)
+      result[i] = reduced_dim(i);
+    return result;
+  }
+
+};
+
+}  // namespace Aperture
+
+#endif  // __CORE_GRID_H_

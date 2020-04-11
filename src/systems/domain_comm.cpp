1379251c736476c50eb3d44b69a01fd53060877d
fizban007 <fizban007@gmail.com>
2020-04-11 10:52:40 -0400

    added domain communicator system

---
+++ src/systems/domain_comm.cpp
@@ -0,0 +1,112 @@
+#include "domain_comm.hpp"
+#include "framework/config.h"
+#include "framework/environment.hpp"
+#include "utils/logger.h"
+
+namespace Aperture {
+
+template <typename Conf>
+void
+domain_comm<Conf>::init() {
+  setup_domain();
+
+  m_env->shared_data().save("domain_info", m_domain_info);
+}
+
+template <typename Conf>
+void
+domain_comm<Conf>::register_callbacks(sim_environment &env) {}
+
+template <typename Conf>
+void
+domain_comm<Conf>::register_dependencies(sim_environment &env) {}
+
+template <typename Conf>
+void
+domain_comm<Conf>::setup_domain() {
+  m_world = MPI_COMM_WORLD;
+  MPI_Comm_rank(m_world, &m_rank);
+  MPI_Comm_size(m_world, &m_size);
+
+  // This is the first place where rank is defined. Tell logger about
+  // this
+  Logger::init(m_rank,
+               (LogLevel)m_env->params().get<int64_t>("log_level", 0));
+
+  auto dims = m_env->params().get<std::vector<int64_t>>("nodes");
+  if (dims.size() < Conf::dim) dims.resize(Conf::dim, 1);
+
+  int64_t total_dim = 1;
+  for (int i = 0; i < Conf::dim; i++) {
+    total_dim *= dims[i];
+  }
+
+  if (total_dim != m_size) {
+    // Given node configuration is not correct, create one on our own
+    Logger::print_err(
+        "Domain decomp in config file does not make sense, generating "
+        "our own.");
+    for (int i = 0; i < Conf::dim; i++) dims[i] = 0;
+
+    MPI_Dims_create(m_size, Conf::dim, m_domain_info.mpi_dims);
+    Logger::err("Created domain decomp as");
+    for (int i = 0; i < Conf::dim; i++) {
+      Logger::err("{}", m_domain_info.mpi_dims[i]);
+      if (i != Conf::dim - 1) Logger::err(" x ");
+    }
+    Logger::err("\n");
+  } else {
+    for (int i = 0; i < Conf::dim; i++)
+      m_domain_info.mpi_dims[i] = dims[i];
+  }
+
+  auto periodic =
+      m_env->params().get<std::vector<bool>>("periodic_boundary");
+  if (periodic.size() == Conf::dim)
+    for (int i = 0; i < Conf::dim; i++)
+      m_domain_info.is_periodic[i] = periodic[i];
+
+  // Create a cartesian MPI group for communication
+  MPI_Cart_create(m_world, Conf::dim, m_domain_info.mpi_dims,
+                  m_domain_info.is_periodic, true, &m_cart);
+
+  // Obtain the mpi coordinate of the current rank
+  MPI_Cart_coords(m_cart, m_rank, Conf::dim, m_domain_info.mpi_coord);
+
+  // Figure out if the current rank is at any boundary
+  int left, right;
+  int rank;
+  for (int n = 0; n < Conf::dim; n++) {
+    MPI_Cart_shift(m_cart, n, -1, &rank, &left);
+    MPI_Cart_shift(m_cart, n, 1, &rank, &right);
+    m_domain_info.neighbor_left[n] = left;
+    m_domain_info.neighbor_right[n] = right;
+    if (left < 0) m_domain_info.is_boundary[2 * n] = true;
+    if (right < 0) m_domain_info.is_boundary[2 * n + 1] = true;
+  }
+
+#ifdef CUDA_ENABLED
+  // Poll the system to detect how many GPUs are on the node, set the GPU
+  // corresponding to the rank
+  int n_devices;
+  cudaGetDeviceCount(&n_devices);
+  if (n_devices <= 0) {
+    std::cerr << "No usable Cuda device found!!" << std::endl;
+    exit(1);
+  }
+  // TODO: This way of finding device id may not be reliable
+  int dev_id = m_rank % n_devices;
+  cudaSetDevice(dev_id);
+#endif
+}
+
+template <typename Conf>
+void
+domain_comm<Conf>::send_guard_cell_vfield(vector_field<Conf> &field) {}
+
+// Explicitly instantiate some of the configurations that may occur
+template class domain_comm<Config<1>>;
+template class domain_comm<Config<2>>;
+template class domain_comm<Config<3>>;
+
+}  // namespace Aperture
---
+++ src/systems/domain_comm.hpp
@@ -0,0 +1,59 @@
+#ifndef __DOMAIN_COMM_H_
+#define __DOMAIN_COMM_H_
+
+#include "data/fields.hpp"
+#include "framework/system.h"
+#include "core/domain_info.h"
+#include "core/particles.h"
+#include <vector>
+#include <mpi.h>
+
+// #define NEIGHBOR_NULL
+
+namespace Aperture {
+
+template <typename Conf>
+class domain_comm : public system_t {
+ private:
+  const Conf& m_conf;
+  MPI_Comm m_world;
+  MPI_Comm m_cart;
+  MPI_Datatype m_scalar_type;
+
+  int m_rank = 0;  ///< Rank of current process
+  int m_size = 1;  ///< Size of MPI_COMM_WORLD
+  domain_info_t<Conf::dim> m_domain_info;
+
+  // Communication buffers
+  std::vector<typename Conf::multi_array_t> m_send_buffers;
+  std::vector<typename Conf::multi_array_t> m_recv_buffers;
+  std::vector<typename Conf::ptc_t> m_ptc_buffers;
+  std::vector<typename Conf::ph_t> m_ph_buffers;
+
+ public:
+  static std::string name() { return "domain communicator"; }
+
+  domain_comm(const Conf& conf) : m_conf(conf) {}
+
+  void init();
+  void update(double, uint32_t) {}
+  void destroy() {}
+  void register_dependencies(sim_environment& env);
+  void register_callbacks(sim_environment& env);
+
+  void setup_domain();
+  void send_guard_cell_vfield(vector_field<Conf>& field);
+  void send_guard_cell_sfield(vector_field<Conf>& field);
+  void get_total_num_offset(uint64_t& num, uint64_t& total,
+                            uint64_t& offset);
+
+  const domain_info_t<Conf::dim>& domain_info() {
+    return m_domain_info;
+  }
+};
+
+}  // namespace Aperture
+
+// #include "domain_comm.cpp"
+
+#endif  // __DOMAIN_COMM_H_

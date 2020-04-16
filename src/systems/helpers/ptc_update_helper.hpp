ebe23a9edeba1b576a6ea901a2f265a736efaabc
fizban007 <fizban007@gmail.com>
2020-04-16 18:09:02 -0400

    added helper for current deposition

---
+++ src/systems/helpers/ptc_update_helper.hpp
@@ -0,0 +1,32 @@
+#ifndef __PTC_UPDATE_HELPER_H_
+#define __PTC_UPDATE_HELPER_H_
+
+#include "core/cuda_control.h"
+
+namespace Aperture {
+
+template <typename FloatT>
+HD_INLINE FloatT
+center2d(FloatT sx0, FloatT sx1, FloatT sy0, FloatT sy1) {
+  return (2.0f * sx1 * sy1 + sx0 * sy1 + sx1 * sy0 + 2.0f * sx0 * sy0) *
+         0.1666667f;
+}
+
+template <typename FloatT>
+HD_INLINE FloatT
+movement3d(FloatT sx0, FloatT sx1, FloatT sy0, FloatT sy1, FloatT sz0,
+           FloatT sz1) {
+  return (sz1 - sz0) * center2d(sx0, sx1, sy0, sy1);
+}
+
+template <typename FloatT>
+HD_INLINE FloatT
+movement2d(FloatT sx0, FloatT sx1, FloatT sy0, FloatT sy1) {
+  return (sy1 - sy0) * 0.5f * (sx0 + sx1);
+}
+
+
+
+}
+
+#endif // __PTC_UPDATE_HELPER_H_

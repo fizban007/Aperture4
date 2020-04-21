d1d9ef26605aeaff536fc813974b4ff719cbfcf7
fizban007 <fizban007@gmail.com>
2020-04-21 14:58:56 -0400

    switch to a problem driven compile model

---
+++ problems/vacuum_sph_wave_test/src/main.cpp
@@ -0,0 +1,27 @@
+#include "framework/config.h"
+#include "framework/environment.hpp"
+#include "systems/field_solver_logsph.h"
+// #include "systems/ptc_updater.h"
+#include "systems/data_exporter.h"
+#include <iostream>
+
+using namespace std;
+using namespace Aperture;
+
+int
+main(int argc, char *argv[]) {
+  typedef Config<2> Conf;
+  sim_environment env(&argc, &argv);
+
+  env.params().add("log_level", (int64_t)LogLevel::debug);
+
+  auto comm = env.register_system<domain_comm<Conf>>(env);
+  auto grid = env.register_system<grid_logsph_t<Conf>>(env, *comm);
+  auto solver =
+      env.register_system<field_solver_logsph<Conf>>(env, *grid, comm);
+  auto exporter = env.register_system<data_exporter<Conf>>(env, *grid, comm);
+
+  env.init();
+  env.run();
+  return 0;
+}

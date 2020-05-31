40df746302cbf1243f49546ae2d7041a7378ae7f
fizban007 <fizban007@gmail.com>
2020-05-31 15:58:00 -0400

    changed interface of finite_diff
    so that it can work with host code too

--- src/systems/helpers/finite_diff_helper.hpp
+++ src/systems/helpers/finite_diff_helper.hpp
@@ -18,72 +18,54 @@ struct finite_diff;
 
 template <>
 struct finite_diff<1> {
-  template <typename PtrType>
-  HD_INLINE static typename PtrType::value_t curl0(
-      const vec_t<PtrType, 3>& f, const typename PtrType::idx_t& idx,
-      const vec_t<stagger_t, 3>& stagger) {
+  template <typename VecType, typename Idx_t, typename Stagger>
+  HD_INLINE static Scalar curl0(const VecType& f, const Idx_t& idx, const Stagger& st) {
     return 0.0;
   }
 
-  template <typename PtrType>
-  HD_INLINE static typename PtrType::value_t curl1(
-      const vec_t<PtrType, 3>& f, const typename PtrType::idx_t& idx,
-      const vec_t<stagger_t, 3>& stagger) {
-    return -diff<0>(f[2], idx, stagger[2]);
+  template <typename VecType, typename Idx_t, typename Stagger>
+  HD_INLINE static Scalar curl1(const VecType& f, const Idx_t& idx, const Stagger& st) {
+    return -diff<0>(f[2], idx, st[2]);
   }
 
-  template <typename PtrType>
-  HD_INLINE static typename PtrType::value_t curl2(
-      const vec_t<PtrType, 3>& f, const typename PtrType::idx_t& idx,
-      const vec_t<stagger_t, 3>& stagger) {
-    return diff<0>(f[1], idx, stagger[1]);
+  template <typename VecType, typename Idx_t, typename Stagger>
+  HD_INLINE static Scalar curl2(const VecType& f, const Idx_t& idx, const Stagger& st) {
+    return diff<0>(f[1], idx, st[1]);
   }
 };
 
 template <>
 struct finite_diff<2> {
-  template <typename PtrType>
-  HD_INLINE static typename PtrType::value_t curl0(
-      const vec_t<PtrType, 3>& f, const typename PtrType::idx_t& idx,
-      const vec_t<stagger_t, 3>& stagger) {
-    return diff<1>(f[2], idx, stagger[2]);
+  template <typename VecType, typename Idx_t, typename Stagger>
+  HD_INLINE static Scalar curl0(const VecType& f, const Idx_t& idx, const Stagger& st) {
+    return diff<1>(f[2], idx, st[2]);
   }
 
-  template <typename PtrType>
-  HD_INLINE static typename PtrType::value_t curl1(
-      const vec_t<PtrType, 3>& f, const typename PtrType::idx_t& idx,
-      const vec_t<stagger_t, 3>& stagger) {
-    return -diff<0>(f[2], idx, stagger[2]);
+  template <typename VecType, typename Idx_t, typename Stagger>
+  HD_INLINE static Scalar curl1(const VecType& f, const Idx_t& idx, const Stagger& st) {
+    return -diff<0>(f[2], idx, st[2]);
   }
 
-  template <typename PtrType>
-  HD_INLINE static typename PtrType::value_t curl2(
-      const vec_t<PtrType, 3>& f, const typename PtrType::idx_t& idx,
-      const vec_t<stagger_t, 3>& stagger) {
-    return diff<0>(f[1], idx, stagger[1]) - diff<1>(f[0], idx, stagger[0]);
+  template <typename VecType, typename Idx_t, typename Stagger>
+  HD_INLINE static Scalar curl2(const VecType& f, const Idx_t& idx, const Stagger& st) {
+    return diff<0>(f[1], idx, st[1]) - diff<1>(f[0], idx, st[0]);
   }
 };
 
 template <>
 struct finite_diff<3> {
-  template <typename PtrType>
-  HD_INLINE static typename PtrType::value_t curl0(
-      const vec_t<PtrType, 3>& f, const typename PtrType::idx_t& idx,
-      const vec_t<stagger_t, 3>& st) {
+  template <typename VecType, typename Idx_t, typename Stagger>
+  HD_INLINE static Scalar curl0(const VecType& f, const Idx_t& idx, const Stagger& st) {
     return diff<1>(f[2], idx, st[2]) - diff<2>(f[1], idx, st[1]);
   }
 
-  template <typename PtrType>
-  HD_INLINE static typename PtrType::value_t curl1(
-      const vec_t<PtrType, 3>& f, const typename PtrType::idx_t& idx,
-      const vec_t<stagger_t, 3>& st) {
+  template <typename VecType, typename Idx_t, typename Stagger>
+  HD_INLINE static Scalar curl1(const VecType& f, const Idx_t& idx, const Stagger& st) {
     return diff<2>(f[0], idx, st[0]) - diff<0>(f[2], idx, st[2]);
   }
 
-  template <typename PtrType>
-  HD_INLINE static typename PtrType::value_t curl2(
-      const vec_t<PtrType, 3>& f, const typename PtrType::idx_t& idx,
-      const vec_t<stagger_t, 3>& st) {
+  template <typename VecType, typename Idx_t, typename Stagger>
+  HD_INLINE static Scalar curl2(const VecType& f, const Idx_t& idx, const Stagger& st) {
     return diff<0>(f[1], idx, st[1]) - diff<1>(f[0], idx, st[0]);
   }
 };

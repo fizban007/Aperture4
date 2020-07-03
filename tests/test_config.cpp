b05f31acddd799db6fc1b7b8b2ea26729b9d9e66
fizban007 <fizban007@gmail.com>
2020-07-03 14:38:10 -0400

    added copyright notice to tests too

--- tests/test_config.cpp
+++ tests/test_config.cpp
@@ -1,5 +1,23 @@
+/*
+ * Copyright (c) 2020 Alex Chen.
+ * This file is part of Aperture (https://github.com/fizban007/Aperture4.git).
+ *
+ * Aperture is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * Aperture is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "catch.hpp"
 #include "framework/config.h"
+#include "utils/type_traits.hpp"
 
 using namespace Aperture;
 
@@ -8,4 +26,16 @@ TEST_CASE("Making multi_array", "[config]") {
   // to contain no output. The test passes as long as this can compile.
   typedef Config<3> Conf;
   auto array = Conf::make_multi_array({32, 32, 32});
+}
+
+TEST_CASE("Multi array is indexable", "[type_traits]") {
+  typedef Config<3> Conf;
+  static_assert(
+      is_indexable<typename Conf::multi_array_t>::value);
+  static_assert(
+      is_const_indexable<typename Conf::multi_array_t>::value);
+  static_assert(
+      is_indexable<typename Conf::ndptr_t>::value);
+  static_assert(
+      is_const_indexable<typename Conf::ndptr_const_t>::value);
 }

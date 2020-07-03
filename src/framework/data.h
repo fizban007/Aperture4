85893b4201e7b2f75aafc4e68e8f927d0b7981ae
fizban007 <fizban007@gmail.com>
2020-07-03 11:54:05 -0400

    added copyright notice to every source file

--- src/framework/data.h
+++ src/framework/data.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2020 Alex Chen.
+ * This file is part of Aperture (https://github.com/fizban007/Aperture4.git).
+ *
+ * Aperture is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * Aperture is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __FRAMEWORK_DATA_H_
 #define __FRAMEWORK_DATA_H_
 

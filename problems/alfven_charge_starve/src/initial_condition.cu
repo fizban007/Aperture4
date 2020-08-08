4e3a54c55e38c7fb49e3c776e5958309a79a1887
fizban007 <fizban007@gmail.com>
2020-08-08 08:43:49 -0400

    improved wave exact solution

--- problems/alfven_charge_starve/src/initial_condition.cu
+++ problems/alfven_charge_starve/src/initial_condition.cu
@@ -58,11 +58,20 @@ struct alfven_wave_solution {
 
   HD_INLINE Scalar wave_arg(Scalar t, Scalar x, Scalar y) const {
     return 2.0 * M_PI *
+           (xi(x, y) - t + eta(x, y) * costh / sinth) / lambda;
+  }
+
+  HD_INLINE Scalar wave_arg_clamped(Scalar t, Scalar x, Scalar y) const {
+    return 2.0 * M_PI *
            clamp<Scalar>((xi(x, y) - t + eta(x, y) * costh / sinth) / lambda,
                          0.0, 1.0);
   }
 
   HD_INLINE Scalar width_arg(Scalar x, Scalar y) const {
+    return (eta(x, y) - eta0) / delta_eta;
+  }
+
+  HD_INLINE Scalar width_arg_clamped(Scalar x, Scalar y) const {
     return clamp<Scalar>((eta(x, y) - eta0) / delta_eta, 0.0, 1.0);
   }
 
@@ -73,7 +82,7 @@ struct alfven_wave_solution {
   }
 
   HD_INLINE Scalar Bz(Scalar t, Scalar x, Scalar y) const {
-    return B0 * math::sin(wave_arg(t, x, y)) * width_prof(width_arg(x, y));
+    return B0 * math::sin(wave_arg_clamped(t, x, y)) * width_prof(width_arg_clamped(x, y));
   }
 
   HD_INLINE Scalar Ex(Scalar t, Scalar x, Scalar y) const {
@@ -86,16 +95,22 @@ struct alfven_wave_solution {
 
   HD_INLINE Scalar Jx(Scalar t, Scalar x, Scalar y) const {
     return -B0 * sinth *
-           (2.0f * M_PI * math::cos(wave_arg(t, x, y)) * costh / lambda /
-                sinth +
-            d_width(width_arg(x, y)));
+           (2.0f * M_PI * math::cos(wave_arg_clamped(t, x, y)) * costh / lambda /
+                sinth * width_prof(width_arg_clamped(x, y)) +
+            math::sin(wave_arg_clamped(t, x, y)) * d_width(width_arg_clamped(x, y)));
   }
 
   HD_INLINE Scalar Jy(Scalar t, Scalar x, Scalar y) const {
     return -B0 * costh *
-           (2.0f * M_PI * math::cos(wave_arg(t, x, y)) * costh / lambda /
-                sinth +
-            d_width(width_arg(x, y)));
+           (2.0f * M_PI * math::cos(wave_arg_clamped(t, x, y)) * costh / lambda /
+                sinth * width_prof(width_arg_clamped(x, y)) +
+            math::sin(wave_arg_clamped(t, x, y)) * d_width(width_arg_clamped(x, y)));
+  }
+
+  HD_INLINE Scalar Rho(Scalar t, Scalar x, Scalar y) const {
+    return -B0 * (2.0f * M_PI * math::cos(wave_arg_clamped(t, x, y)) * costh / lambda /
+                sinth * width_prof(width_arg_clamped(x, y)) +
+            math::sin(wave_arg_clamped(t, x, y)) * d_width(width_arg_clamped(x, y)));
   }
 };
 
@@ -180,128 +195,193 @@ initial_condition_wave(sim_environment &env, vector_field<Conf> &B,
   E.set_values(
       1, [wave](Scalar x, Scalar y, Scalar z) { return wave.Ey(0.0, x, y); });
 
+  auto num = ptc.number();
   // Compute injection number per cell
-  auto ext = B.grid().extent();
-  multi_array<int, Conf::dim> num_per_cell(ext, MemType::host_device);
-  multi_array<int, Conf::dim> cum_num_per_cell(ext, MemType::host_device);
+  // auto ext = B.grid().extent();
+  // multi_array<int, Conf::dim> num_per_cell(ext, MemType::host_device);
+  // multi_array<int, Conf::dim> cum_num_per_cell(ext, MemType::host_device);
 
-  num_per_cell.assign_dev(0);
-  cum_num_per_cell.assign_dev(0);
+  // num_per_cell.assign_dev(0);
+  // cum_num_per_cell.assign_dev(0);
 
-  // Every cell with nonzero j gets mult number of pairs
-  kernel_launch(
-      [wave, ext] __device__(auto num_per_cell, auto mult) {
-        auto& grid = dev_grid<Conf::dim>();
-        for (auto idx : grid_stride_range(Conf::begin(ext), Conf::end(ext))) {
-          auto pos = idx.get_pos();
-          auto x = grid.template pos<0>(pos[0], 0.5f);
-          auto y = grid.template pos<1>(pos[1], 0.5f);
+  // // Every cell with nonzero j gets mult number of pairs
+  // kernel_launch(
+  //     [wave, ext] __device__(auto num_per_cell, auto mult) {
+  //       auto& grid = dev_grid<Conf::dim>();
+  //       for (auto idx : grid_stride_range(Conf::begin(ext), Conf::end(ext))) {
+  //         auto pos = idx.get_pos();
+  //         auto x = grid.template pos<0>(pos[0], 0.5f);
+  //         auto y = grid.template pos<1>(pos[1], 0.5f);
 
-          auto jx = wave.Jx(0.0, x, y);
-          auto jy = wave.Jy(0.0, x, y);
-          auto j = math::sqrt(jx * jx + jy * jy);
+  //         // auto jx = wave.Jx(0.0, x, y);
+  //         // auto jy = wave.Jy(0.0, x, y);
+  //         // auto j = math::sqrt(jx * jx + jy * jy);
+  //         auto width_arg = wave.width_arg(x, y);
+  //         auto wave_arg = wave.wave_arg(0.0f, x, y);
 
-          if (j > TINY) {
-            num_per_cell[idx] = mult;
-          }
-        }
-      }, num_per_cell.dev_ndptr(), mult);
-  CudaCheckError();
+  //         if (wave_arg > 0.0f && wave_arg < 2.0f * M_PI &&
+  //             width_arg > 0.0f && width_arg < 1.0f) {
+  //           num_per_cell[idx] = mult;
+  //         }
+  //       }
+  //     }, num_per_cell.dev_ndptr(), mult);
+  // CudaCheckError();
 
-  thrust::device_ptr<int> p_num_per_cell(num_per_cell.dev_ptr());
-  thrust::device_ptr<int> p_cum_num_per_cell(cum_num_per_cell.dev_ptr());
+  // thrust::device_ptr<int> p_num_per_cell(num_per_cell.dev_ptr());
+  // thrust::device_ptr<int> p_cum_num_per_cell(cum_num_per_cell.dev_ptr());
 
-  thrust::exclusive_scan(p_num_per_cell, p_num_per_cell + ext.size(),
-                         p_cum_num_per_cell);
-  CudaCheckError();
-  num_per_cell.copy_to_host();
-  cum_num_per_cell.copy_to_host();
-  int new_pairs = 2 * (cum_num_per_cell[ext.size() - 1] + num_per_cell[ext.size() - 1]);
+  // thrust::exclusive_scan(p_num_per_cell, p_num_per_cell + ext.size(),
+  //                        p_cum_num_per_cell);
+  // CudaCheckError();
+  // num_per_cell.copy_to_host();
+  // cum_num_per_cell.copy_to_host();
+  // int new_pairs = 2 * (cum_num_per_cell[ext.size() - 1] + num_per_cell[ext.size() - 1]);
+  // Logger::print_info("Injecting {} pairs", new_pairs);
 
   // Actually inject particles
-  auto num = ptc.number();
-  kernel_launch(
-      [num, mult, q_e, weight, wave] __device__(auto ptc, auto states, auto num_per_cell,
-                                                auto cum_num_per_cell) {
-        auto &grid = dev_grid<Conf::dim>();
-        auto ext = grid.extent();
-        int id = threadIdx.x + blockIdx.x * blockDim.x;
-        cuda_rng_t rng(&states[id]);
-        for (auto cell : grid_stride_range(0, ext.size())) {
-          auto idx = Conf::idx(cell, ext);
-          auto pos = idx.get_pos();
-          for (int i = 0; i < num_per_cell[cell]; i++) {
-            int offset = num + cum_num_per_cell[cell] * 2 + i * 2;
-            auto x1 = rng();
-            auto x2 = rng();
-            ptc.x1[offset] = ptc.x1[offset + 1] = x1;
-            ptc.x2[offset] = ptc.x2[offset + 1] = x2;
-            ptc.x3[offset] = ptc.x3[offset + 1] = 0.0f;
-            ptc.cell[offset] = ptc.cell[offset + 1] = cell;
-            ptc.weight[offset] = ptc.weight[offset + 1] = weight;
-            ptc.flag[offset] = set_ptc_type_flag(0, PtcType::electron);
-            ptc.flag[offset + 1] = set_ptc_type_flag(0, PtcType::positron);
-
-            auto x = grid.template pos<0>(pos[0], x1);
-            auto y = grid.template pos<1>(pos[1], x2);
-            auto jx = wave.Jx(0.0f, x, y);
-            auto jy = wave.Jy(0.0f, x, y);
-            auto j = math::sqrt(jx * jx + jy * jy);
-            auto v = j / (q_e * mult);
-            auto gamma = 1.0f / math::sqrt(1.0f - v * v);
-            ptc.p1[offset] = -jx * gamma / (q_e * mult);
-            ptc.p2[offset] = -jy * gamma / (q_e * mult);
-            ptc.p3[offset] = 0.0f;
-            ptc.E[offset] = gamma;
-            ptc.p1[offset + 1] = 0.0f;
-            ptc.p2[offset + 1] = 0.0f;
-            ptc.p3[offset + 1] = 0.0f;
-            ptc.E[offset + 1] = 1.0f;
-          }
-        }
-      }, ptc.dev_ptrs(), states.states(), num_per_cell.dev_ndptr_const(),
-      cum_num_per_cell.dev_ndptr_const());
-  CudaSafeCall(cudaDeviceSynchronize());
-  ptc.set_num(num + new_pairs);
-
-  // auto num = ptc.number();
   // kernel_launch(
-  //     [num, mult, q_e] __device__(auto ptc, auto states) {
+  //     [num, mult, q_e, weight, wave] __device__(auto ptc, auto states, auto num_per_cell,
+  //                                               auto cum_num_per_cell) {
   //       auto &grid = dev_grid<Conf::dim>();
   //       auto ext = grid.extent();
   //       int id = threadIdx.x + blockIdx.x * blockDim.x;
   //       cuda_rng_t rng(&states[id]);
-  //       for (auto n : grid_stride_range(0, ext.size())) {
-  //         auto idx = idx_col_major_t<Conf::dim>(n, ext);
+  //       for (auto cell : grid_stride_range(0, ext.size())) {
+  //         auto idx = Conf::idx(cell, ext);
   //         auto pos = idx.get_pos();
-  //         auto idx_row = idx_row_major_t<Conf::dim>(pos, ext);
-  //         if (pos[0] > grid.dims[0] / 2) continue;
-  //         if (grid.is_in_bound(pos)) {
-  //           for (int i = 0; i < mult; i++) {
-  //             uint32_t offset = num + idx_row.linear * mult * 2 + i * 2;
+  //         for (int i = 0; i < num_per_cell[cell]; i++) {
+  //           int offset = num + cum_num_per_cell[cell] * 2 + i * 2;
+  //           auto x1 = rng();
+  //           auto x2 = rng();
+  //           ptc.x1[offset] = ptc.x1[offset + 1] = x1;
+  //           ptc.x2[offset] = ptc.x2[offset + 1] = x2;
+  //           ptc.x3[offset] = ptc.x3[offset + 1] = 0.0f;
+  //           ptc.cell[offset] = ptc.cell[offset + 1] = cell;
+  //           ptc.weight[offset] = ptc.weight[offset + 1] = weight;
+  //           ptc.flag[offset] = set_ptc_type_flag(0, PtcType::electron);
+  //           ptc.flag[offset + 1] = set_ptc_type_flag(0, PtcType::positron);
 
-  //             ptc.x1[offset] = ptc.x1[offset + 1] = rng();
-  //             ptc.x2[offset] = ptc.x2[offset + 1] = rng();
-  //             ptc.x3[offset] = ptc.x3[offset + 1] = rng();
-  //             ptc.p1[offset] = ptc.p1[offset + 1] = 0.0;
-  //             ptc.p2[offset] = ptc.p2[offset + 1] = 0.0;
-  //             ptc.p3[offset] = ptc.p3[offset + 1] = 0.0;
-  //             ptc.E[offset] = ptc.E[offset + 1] = 1.0;
-  //             ptc.cell[offset] = ptc.cell[offset + 1] = idx.linear;
-  //             Scalar x = grid.template pos<0>(pos[0], ptc.x1[offset]);
-  //             ptc.weight[offset] = ptc.weight[offset + 1] = cube(
-  //                 math::abs(0.5f * grid.sizes[0] - x) * 2.0f / grid.sizes[0]);
-  //             ptc.flag[offset] = set_ptc_type_flag(flag_or(PtcFlag::primary),
-  //                                                  PtcType::electron);
-  //             ptc.flag[offset + 1] = set_ptc_type_flag(
-  //                 flag_or(PtcFlag::primary), PtcType::positron);
-  //           }
+  //           auto x = grid.template pos<0>(pos[0], x1);
+  //           auto y = grid.template pos<1>(pos[1], x2);
+  //           auto jx = wave.Jx(0.0f, x, y);
+  //           auto jy = wave.Jy(0.0f, x, y);
+  //           auto j = math::sqrt(jx * jx + jy * jy);
+  //           auto v = j / (q_e * mult);
+  //           auto gamma = 1.0f / math::sqrt(1.0f - v * v);
+  //           ptc.p1[offset] = -jx * gamma / (q_e * mult);
+  //           ptc.p2[offset] = -jy * gamma / (q_e * mult);
+  //           ptc.p3[offset] = 0.0f;
+  //           ptc.E[offset] = gamma;
+  //           ptc.p1[offset + 1] = 0.0f;
+  //           ptc.p2[offset + 1] = 0.0f;
+  //           ptc.p3[offset + 1] = 0.0f;
+  //           ptc.E[offset + 1] = 1.0f;
   //         }
   //       }
-  //     },
-  //     ptc.dev_ptrs(), states.states());
+  //     }, ptc.dev_ptrs(), states.states(), num_per_cell.dev_ndptr_const(),
+  //     cum_num_per_cell.dev_ndptr_const());
   // CudaSafeCall(cudaDeviceSynchronize());
-  // ptc.set_num(num + mult * 2 * B0.grid().extent().size() / 2);
+  // ptc.set_num(num + new_pairs);
+
+  num = ptc.number();
+  kernel_launch(
+      [mult, num, q_e, wave, Bp] __device__(auto ptc, auto states, auto w) {
+        // int mult = 1;
+        auto &grid = dev_grid<Conf::dim>();
+        auto ext = grid.extent();
+        int id = threadIdx.x + blockIdx.x * blockDim.x;
+        cuda_rng_t rng(&states[id]);
+        for (auto n : grid_stride_range(0, ext.size())) {
+          auto idx = idx_col_major_t<Conf::dim>(n, ext);
+          auto pos = idx.get_pos();
+          auto idx_row = idx_row_major_t<Conf::dim>(pos, ext);
+          if (pos[0] > grid.dims[0] / 2) continue;
+          if (grid.is_in_bound(pos)) {
+            for (int i = 0; i < mult; i++) {
+              uint32_t offset = num + idx_row.linear * mult * 2 + i * 2;
+              ptc.x1[offset] = ptc.x1[offset + 1] = rng();
+              ptc.x2[offset] = ptc.x2[offset + 1] = rng();
+              ptc.x3[offset] = ptc.x3[offset + 1] = 0.0f;
+
+              Scalar x = grid.template pos<0>(pos[0], ptc.x1[offset]);
+              Scalar y = grid.template pos<1>(pos[1], ptc.x2[offset]);
+              auto width_arg = wave.width_arg(x, y);
+              auto wave_arg = wave.wave_arg(0.0f, x, y);
+
+              Scalar weight = w * cube(
+                  math::abs(0.5f * grid.sizes[0] - x) * 2.0f / grid.sizes[0]);
+              if (wave_arg > 0.0f && wave_arg < 2.0f * M_PI &&
+                  width_arg > 0.0f && width_arg < 1.0f) {
+
+                auto rho = wave.Rho(0.0f, x, y);
+                // auto jx = rho * wave.sinth;
+                // auto jy = rho * wave.costh;
+                // auto jy = wave.Jy(0.0f, x, y);
+                // auto j = math::sqrt(jx * jx + jy * jy);
+                // auto v = j / (2.0f * q_e * mult);
+                // auto v = math::abs(rho) / (2.0f * q_e * mult * weight);
+                auto v = math::abs(rho) / (weight + math::abs(rho) * 3.0f / (q_e * mult)) / (q_e * mult);
+                // auto v3 = math::abs(rho) * 3.0f / (2.0f * q_e * mult * weight);
+                auto v_d = wave.B0 / Bp;
+                // auto v_d = 0.0f;
+                auto gamma = 1.0f / math::sqrt(1.0f - v * v - v_d * v_d);
+                // auto gamma3 = 1.0f / math::sqrt(1.0f - v3 * v3 - v_d * v_d);
+                // auto sgn_jy = sgn(jy);
+
+                if (rho > 0.0) {
+                  ptc.p1[offset] = 0.0f;
+                  ptc.p1[offset + 1] = v * wave.sinth * gamma;
+                  ptc.p2[offset] = 0.0f;
+                  ptc.p2[offset + 1] = v * wave.costh * gamma;
+                  ptc.p3[offset] = 0.0f;
+                  ptc.p3[offset + 1] = v_d * gamma;
+                  ptc.E[offset] = 1.0f;
+                  ptc.E[offset + 1] = gamma;
+                  ptc.weight[offset] = weight + math::abs(rho) * 2.0f / (q_e * mult);
+                  ptc.weight[offset + 1] = weight + math::abs(rho) * 3.0f / (q_e * mult);
+                } else {
+                  ptc.p1[offset] = v * wave.sinth * gamma;
+                  ptc.p1[offset + 1] = 0.0f;
+                  ptc.p2[offset] = v * wave.costh * gamma;
+                  ptc.p2[offset + 1] = 0.0f;
+                  ptc.p3[offset] = v_d * gamma;
+                  ptc.p3[offset + 1] = 0.0f;
+                  ptc.E[offset] = gamma;
+                  ptc.E[offset + 1] = 1.0f;
+                  ptc.weight[offset] = weight + math::abs(rho) * 3.0f / (q_e * mult);
+                  ptc.weight[offset + 1] = weight + math::abs(rho) * 2.0f / (q_e * mult);
+                }
+                // ptc.p1[offset] = -jx * gamma / (2.0f * q_e * mult * weight);
+                // ptc.p1[offset + 1] = jx * gamma / (2.0f * q_e * mult * weight);
+                // ptc.p2[offset] = -jy * gamma / (2.0f * q_e * mult * weight);
+                // ptc.p2[offset + 1] = jy * gamma / (2.0f * q_e * mult * weight);
+                // ptc.p3[offset] = v_d * gamma;
+                // ptc.p3[offset + 1] = v_d * gamma;
+                // ptc.E[offset] = gamma;
+                // ptc.E[offset + 1] = gamma;
+                // ptc.weight[offset] = ptc.weight[offset + 1] = weight;
+              } else {
+                ptc.p1[offset] = ptc.p1[offset + 1] = 0.0f;
+                ptc.p2[offset] = ptc.p2[offset + 1] = 0.0f;
+                ptc.p3[offset] = ptc.p3[offset + 1] = 0.0f;
+                ptc.E[offset] = ptc.E[offset + 1] = 1.0f;
+                ptc.weight[offset] = ptc.weight[offset + 1] = weight;
+              }
+
+              ptc.cell[offset] = ptc.cell[offset + 1] = idx.linear;
+
+              // Scalar x = grid.template pos<0>(pos[0], ptc.x1[offset]);
+              ptc.flag[offset] = set_ptc_type_flag(flag_or(PtcFlag::primary),
+                                                   PtcType::electron);
+              ptc.flag[offset + 1] = set_ptc_type_flag(
+                  flag_or(PtcFlag::primary), PtcType::positron);
+            }
+          }
+        }
+      },
+      ptc.dev_ptrs(), states.states(), weight);
+  CudaSafeCall(cudaDeviceSynchronize());
+  ptc.set_num(num + 2 * mult * B0.grid().extent().size() / 2);
 }
 
 template void set_initial_condition<Config<2>>(sim_environment &env,

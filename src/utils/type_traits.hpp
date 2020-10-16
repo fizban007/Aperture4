837735b06e58b0c378b3e478233e886774192865
fizban007 <fizban007@gmail.com>
2020-10-16 13:23:31 -0400

    added a simple less_than template utility

--- src/utils/type_traits.hpp
+++ src/utils/type_traits.hpp
@@ -22,6 +22,11 @@
 
 namespace Aperture {
 
+template <int A, int B>
+struct less_than {
+  enum { value = A < B };
+};
+
 // This implementation of conjunction is taken from
 // https://www.fluentcpp.com/2019/01/25/variadic-number-function-parameters-type/
 // In c++17 there is std::conjunction, but this implementation is to keep

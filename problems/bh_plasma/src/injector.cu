75be02eee0be807aabfcedf2f0565ec1cef423e1
fizban007 <fizban007@gmail.com>
2020-12-17 18:12:12 -0500

    adding a plasma injector for bh simulations

---
+++ problems/bh_plasma/src/injector.cu
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2020 Alex Chen.
+ * This file is part of Aperture (https://github.com/fizban007/Aperture4.git).
+ *
+ * Aperture is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * Aperture is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "framework/config.h"
+#include "injector.h"
+
+namespace Aperture {
+
+template <typename Conf> void bh_injector<Conf>::register_data_components() {}
+
+template <typename Conf> void bh_injector<Conf>::init() {
+  m_env.get_data("E", &D);
+  m_env.get_data("B", &B);
+  m_env.get_data("particles", &ptc);
+}
+
+template <typename Conf>
+void bh_injector<Conf>::update(double dt, uint32_t step) {
+  value_t a = m_grid.a;
+}
+
+template class bh_injector<Config<2, float>>;
+template class bh_injector<Config<2, double>>;
+
+} // namespace Aperture
---
+++ problems/bh_plasma/src/injector.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020 Alex Chen.
+ * This file is part of Aperture (https://github.com/fizban007/Aperture4.git).
+ *
+ * Aperture is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * Aperture is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _BH_INJECTOR_H_
+#define _BH_INJECTOR_H_
+
+#include "data/fields.h"
+#include "data/particle_data.h"
+#include "framework/system.h"
+#include "systems/grid_ks.h"
+
+namespace Aperture {
+
+template <typename Conf>
+class bh_injector : public system_t {
+ public:
+  using value_t = typename Conf::value_t;
+  static std::string name() { return "bh_injector"; }
+
+  bh_injector(sim_environment& env, const grid_ks_t<Conf>& grid)
+      : system_t(env), m_grid(grid) {}
+  virtual ~bh_injector() {}
+
+  void init() override;
+  void update(double dt, uint32_t step) override;
+  void register_data_components() override;
+
+ private:
+  const grid_ks_t<Conf>& m_grid;
+  particle_data_t* ptc;
+  vector_field<Conf>* B, D;
+};
+
+}
+
+#endif  // _BH_INJECTOR_H_

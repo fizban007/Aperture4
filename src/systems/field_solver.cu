87c1a99a68933f93ac94367319e6fb065c0a977f
fizban007 <fizban007@gmail.com>
2020-12-21 17:49:19 -0500

    new semi-implicit solver

--- src/systems/field_solver.cu
+++ src/systems/field_solver.cu
@@ -22,12 +22,14 @@
 #include "field_solver.h"
 #include "framework/config.h"
 #include "systems/helpers/finite_diff_helper.hpp"
-// #include "systems/helpers/field_solver_helper_cu.hpp"
+#include "systems/helpers/field_solver_helper_cu.hpp"
 #include "utils/double_buffer.h"
 #include "utils/kernel_helper.hpp"
 #include "utils/timer.h"
 
 namespace Aperture {
+
+namespace {
 
 template <typename Conf>
 using fd = finite_diff<Conf::dim, 2>;
@@ -38,31 +40,32 @@ constexpr float cherenkov_factor = 1.0f;
 template <typename Conf>
 void
 compute_e_update_explicit_cu(vector_field<Conf>& result,
+                             const vector_field<Conf>& e,
                              const vector_field<Conf>& b,
                              const vector_field<Conf>& j,
                              typename Conf::value_t dt) {
   kernel_launch(
-      [dt] __device__(auto result, auto b, auto stagger, auto j) {
+      [dt] __device__(auto result, auto e, auto b, auto stagger, auto j) {
         auto& grid = dev_grid<Conf::dim, typename Conf::value_t>();
         auto ext = grid.extent();
         for (auto idx : grid_stride_range(Conf::begin(ext), Conf::end(ext))) {
           auto pos = idx.get_pos();
           if (grid.is_in_bound(pos)) {
-            result[0][idx] +=
+            result[0][idx] = e[0][idx] +
                 dt *
                 (cherenkov_factor * fd<Conf>::curl0(b, idx, stagger, grid) - j[0][idx]);
 
-            result[1][idx] +=
+            result[1][idx] = e[1][idx] +
                 dt *
                 (cherenkov_factor * fd<Conf>::curl1(b, idx, stagger, grid) - j[1][idx]);
 
-            result[2][idx] +=
+            result[2][idx] = e[2][idx] +
                 dt *
                 (cherenkov_factor * fd<Conf>::curl2(b, idx, stagger, grid) - j[2][idx]);
           }
         }
       },
-      result.get_ptrs(), b.get_ptrs(), b.stagger_vec(), j.get_ptrs());
+      result.get_ptrs(), e.get_ptrs(), b.get_ptrs(), b.stagger_vec(), j.get_ptrs());
   CudaSafeCall(cudaDeviceSynchronize());
   CudaCheckError();
 }
@@ -70,27 +73,28 @@ compute_e_update_explicit_cu(vector_field<Conf>& result,
 template <typename Conf>
 void
 compute_b_update_explicit_cu(vector_field<Conf>& result,
+                             const vector_field<Conf>& b,
                              const vector_field<Conf>& e,
                              typename Conf::value_t dt) {
   kernel_launch(
-      [dt] __device__(auto result, auto e, auto stagger) {
+      [dt] __device__(auto result, auto b, auto e, auto stagger) {
         auto& grid = dev_grid<Conf::dim, typename Conf::value_t>();
         auto ext = grid.extent();
         for (auto idx : grid_stride_range(Conf::begin(ext), Conf::end(ext))) {
           auto pos = idx.get_pos();
           if (grid.is_in_bound(pos)) {
-            result[0][idx] +=
+            result[0][idx] = b[0][idx]
                 -dt * cherenkov_factor * fd<Conf>::curl0(e, idx, stagger, grid);
 
-            result[1][idx] +=
+            result[1][idx] = b[1][idx]
                 -dt * cherenkov_factor * fd<Conf>::curl1(e, idx, stagger, grid);
 
-            result[2][idx] +=
+            result[2][idx] = b[2][idx]
                 -dt * cherenkov_factor * fd<Conf>::curl2(e, idx, stagger, grid);
           }
         }
       },
-      result.get_ptrs(), e.get_ptrs(), e.stagger_vec());
+      result.get_ptrs(), b.get_ptrs(), e.get_ptrs(), e.stagger_vec());
   CudaSafeCall(cudaDeviceSynchronize());
   CudaCheckError();
 }
@@ -159,8 +163,8 @@ template <typename Conf>
 void
 compute_divs_cu(scalar_field<Conf>& divE, scalar_field<Conf>& divB,
                 const vector_field<Conf>& e, const vector_field<Conf>& b,
-                const bool is_boundary[Conf::dim * 2]) {
-  vec_t<bool, Conf::dim * 2> boundary(is_boundary);
+                const vec_t<bool, Conf::dim * 2> &is_boundary) {
+  // vec_t<bool, Conf::dim * 2> boundary(is_boundary);
   kernel_launch(
       [] __device__(auto divE, auto divB, auto e, auto b, auto st_e, auto st_b,
                     auto is_boundary) {
@@ -185,7 +189,7 @@ compute_divs_cu(scalar_field<Conf>& divE, scalar_field<Conf>& divB,
         }
       },
       divE.dev_ndptr(), divB.dev_ndptr(), e.get_ptrs(), b.get_ptrs(),
-      e.stagger_vec(), b.stagger_vec(), boundary);
+      e.stagger_vec(), b.stagger_vec(), is_boundary);
   CudaSafeCall(cudaDeviceSynchronize());
   CudaCheckError();
 }
@@ -206,6 +210,8 @@ clean_field(vector_field<Conf>& f) {
     }, f.get_ptrs());
   CudaSafeCall(cudaDeviceSynchronize());
   CudaCheckError();
+}
+
 }
 
 template <typename Conf>
@@ -232,18 +238,18 @@ void
 field_solver_cu<Conf>::update_explicit(double dt, double time) {
   // dt *= 1.025;
   if (time < TINY) {
-    compute_e_update_explicit_cu(*(this->E), *(this->B), *(this->J), 0.5f * dt);
+    compute_e_update_explicit_cu(*(this->E), *(this->E), *(this->B), *(this->J), 0.5f * dt);
     if (this->m_comm != nullptr) this->m_comm->send_guard_cells(*(this->E));
   }
 
   if (this->m_update_b) {
-    compute_b_update_explicit_cu(*(this->B), *(this->E), dt);
+    compute_b_update_explicit_cu(*(this->B), *(this->B), *(this->E), dt);
     // Communicate the new B values to guard cells
     if (this->m_comm != nullptr) this->m_comm->send_guard_cells(*(this->B));
   }
 
   if (this->m_update_e) {
-    compute_e_update_explicit_cu(*(this->E), *(this->B), *(this->J), dt);
+    compute_e_update_explicit_cu(*(this->E), *(this->E), *(this->B), *(this->J), dt);
     // Communicate the new E values to guard cells
     if (this->m_comm != nullptr) this->m_comm->send_guard_cells(*(this->E));
   }
@@ -252,7 +258,9 @@ field_solver_cu<Conf>::update_explicit(double dt, double time) {
     compute_divs_cu(*(this->divE), *(this->divB), *(this->E), *(this->B),
                     this->m_comm->domain_info().is_boundary);
   } else {
-    bool is_boundary[4] = {true, true, true, true};
+    // bool is_boundary[4] = {true, true, true, true};
+    vec_t<bool, Conf::dim * 2> is_boundary;
+    is_boundary = true;
     compute_divs_cu(*(this->divE), *(this->divB), *(this->E), *(this->B),
                     is_boundary);
   }
@@ -307,7 +315,7 @@ field_solver_cu<Conf>::update_semi_implicit_old(double dt, double alpha,
 
   // buffer.main() now holds alpha*B^n + beta*B^{n+1}. Compute E explicitly from
   // this
-  compute_e_update_explicit_cu(*(this->E), buffer.main(), *(this->J), dt);
+  compute_e_update_explicit_cu(*(this->E), *(this->E), buffer.main(), *(this->J), dt);
 
   // Communicate E
   if (this->m_comm != nullptr) this->m_comm->send_guard_cells(*(this->E));
@@ -321,7 +329,9 @@ field_solver_cu<Conf>::update_semi_implicit_old(double dt, double alpha,
     compute_divs_cu(*(this->divE), *(this->divB), *(this->E), *(this->B),
                     this->m_comm->domain_info().is_boundary);
   } else {
-    bool is_boundary[4] = {true, true, true, true};
+    // bool is_boundary[4] = {true, true, true, true};
+    vec_t<bool, Conf::dim * 2> is_boundary;
+    is_boundary = true;
     compute_divs_cu(*(this->divE), *(this->divB), *(this->E), *(this->B),
                     is_boundary);
   }
@@ -331,6 +341,46 @@ template <typename Conf>
 void
 field_solver_cu<Conf>::update_semi_implicit(double dt, double alpha,
                                             double beta, double time) {
+  compute_e_update_explicit_cu(*(this->m_enew), *(this->E), *(this->B), *(this->J), dt);
+  compute_b_update_explicit_cu(*(this->m_bnew), *(this->B), *(this->E), dt);
+
+  if (this->m_comm != nullptr) {
+    this->m_comm->send_guard_cells(*(this->m_enew));
+    this->m_comm->send_guard_cells(*(this->m_bnew));
+  }
+
+  // Start iterating a few times
+  const int n_iteration = 4;
+  for (int i = 0; i < n_iteration; i++) {
+    add_alpha_beta_cu(*(this->m_tmp_e1), *(this->E), *(this->m_enew), alpha, beta);
+    add_alpha_beta_cu(*(this->m_tmp_b1), *(this->B), *(this->m_bnew), alpha, beta);
+
+    compute_e_update_explicit_cu(*(this->m_enew), *(this->E), *(this->m_tmp_b1),
+                                 *(this->J), dt);
+    compute_b_update_explicit_cu(*(this->m_bnew), *(this->B), *(this->m_tmp_e1), dt);
+
+    if (this->m_comm != nullptr) {
+      this->m_comm->send_guard_cells(*(this->m_enew));
+      this->m_comm->send_guard_cells(*(this->m_bnew));
+    }
+  }
+
+  this->E->copy_from(*(this->m_enew));
+  this->B->copy_from(*(this->m_bnew));
+
+  if (this->m_comm != nullptr) {
+    vec_t<bool, Conf::dim * 2> is_boundary(this->m_comm->domain_info().is_boundary);
+    compute_divs_cu(*(this->divE), *(this->divB), *(this->E), *(this->B),
+                    is_boundary);
+  } else {
+    // bool is_boundary[4] = {true, true, true, true};
+    vec_t<bool, Conf::dim * 2> is_boundary;
+    is_boundary = true;
+    compute_divs_cu(*(this->divE), *(this->divB), *(this->E), *(this->B),
+                    is_boundary);
+  }
+
+  CudaSafeCall(cudaDeviceSynchronize());
 }
 
 INSTANTIATE_WITH_CONFIG(field_solver_cu);

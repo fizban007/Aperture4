23d1416c9d40b66d552273e1c71b6d6a32689423
fizban007 <fizban007@gmail.com>
2021-02-16 15:31:57 -0500

    temporarily disabled extra field keys in datalib
    They are not useful in GR data analysis

--- python/datalib.py
+++ python/datalib.py
@@ -39,7 +39,8 @@ class Data:
         os.path.join(self._path, f"fld.{self._current_fld_step:05d}.h5"),
         "r",
       )
-      self._fld_keys = list(f_fld.keys()) + extra_fld_keys
+      # self._fld_keys = list(f_fld.keys()) + extra_fld_keys
+      self._fld_keys = list(f_fld.keys())
       print(self._fld_keys)
       f_fld.close()
 
@@ -83,16 +84,16 @@ class Data:
 
   def __load_fld_quantity(self, key):
     path = os.path.join(self._path, f"fld.{self._current_fld_step:05d}.h5")
-    if key == "flux":
-      self.__dict__[key] = np.cumsum(self.B1,axis=0)*self.delta[1] - np.cumsum(self.B2,axis=1)*self.delta[0]
-    elif key == "B":
-      self.__dict__[key] = np.sqrt(self.B1 * self.B1 + self.B2 * self.B2 + self.B3 * self.B3)
-    elif key == "J":
-      self.__dict__[key] = np.sqrt(self.J1 * self.J1 + self.J2 * self.J2 + self.J3 * self.J3)
-    else:
-      data = h5py.File(path, "r")
-      self.__dict__[key] = data[key][()]
-      data.close()
+    # if key == "flux":
+    #   self.__dict__[key] = np.cumsum(self.B1,axis=0)*self.delta[1] - np.cumsum(self.B2,axis=1)*self.delta[0]
+    # elif key == "B":
+    #   self.__dict__[key] = np.sqrt(self.B1 * self.B1 + self.B2 * self.B2 + self.B3 * self.B3)
+    # elif key == "J":
+    #   self.__dict__[key] = np.sqrt(self.J1 * self.J1 + self.J2 * self.J2 + self.J3 * self.J3)
+    # else:
+    data = h5py.File(path, "r")
+    self.__dict__[key] = data[key][()]
+    data.close()
 
   def __load_ptc_quantity(self, key):
     pass

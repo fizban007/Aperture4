3efaca6e99bd13d4103efb2fbcff072df1dc4b15
fizban007 <fizban007@gmail.com>
2021-04-22 18:13:20 -0400

    new standalone mpi test

--- tests/test_summit_mpi.cu
+++ tests/test_summit_mpi.cu
@@ -15,41 +15,75 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "core/particles.h"
-#include "framework/environment.h"
-#include "framework/config.h"
-#include "systems/grid.h"
-#include "systems/domain_comm_async.h"
-#include "utils/logger.h"
+#include <iostream>
+#include <vector>
+#include "mpi.h"
 
-using namespace Aperture;
+__global__ void set_mem(double* buffer, int size, double value) {
+  for (int n = threadIdx.x + blockIdx.x * blockDim.x;
+       n < size;
+       n += gridDim.x * blockDim.x) {
+    buffer[n] = value;
+  }
+}
 
 int
 main(int argc, char* argv[]) {
-  // sim_environment env(&argc, &argv);
-  auto& env = sim_environment::instance(&argc, &argv, true);
-  typedef Config<3> Conf;
+  MPI_Init(NULL, NULL);
+  
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  env.params().add("log_level", int64_t(LogLevel::debug));
-  env.params().add("N", std::vector<int64_t>({100, 100, 100}));
-  env.params().add("guard", std::vector<int64_t>({2, 2, 2}));
-  env.params().add("nodes", std::vector<int64_t>({2, 2, 2}));
-  env.params().add("lower", std::vector<double>({1.0, 2.0, 0.0}));
-  env.params().add("size", std::vector<double>({100.0, 10.0, 30.0}));
-  env.params().add("periodic_boundary", std::vector<bool>({true, true, true}));
-  env.params().add("ptc_buffer_size", int64_t(100));
-  env.params().add("ph_buffer_size", int64_t(100));
+  std::cout << "rank is: " << rank << ", total size is: " << size << std::endl;
 
-  auto comm = env.register_system<domain_comm_async<Conf>>();
-  auto grid = env.register_system<grid_t<Conf>>(*comm);
-
-  typename Conf::multi_array_t v(grid->extent());
-  v.assign_dev(comm->rank());
-  size_t free_mem, total_mem;
-  for (int i = 0; i < 100; i++) {
-    comm->send_guard_cells(v, *grid);
-    cudaMemGetInfo( &free_mem, &total_mem );
-    Logger::print_info("GPU memory: free = {} GiB, total = {} GiB", free_mem/1.0e9, total_mem/1.0e9);
+  int n_devices;
+  cudaGetDeviceCount(&n_devices);
+  if (n_devices <= 0) {
+    std::cerr << "No usable Cuda device found!!" << std::endl;
+    exit(1);
+  } else {
+    std::cout << "Found " << n_devices << " Cuda devices!" << std::endl;
   }
-  v.copy_to_host();
+  // set device number
+  int dev_id = rank % n_devices;
+  std::cout << "Rank " << rank << " is on device #" << dev_id << std::endl;
+  cudaSetDevice(dev_id);
+
+  // Prepare the buffer for communication
+  double* dev_send_buffer;
+  double* dev_recv_buffer;
+  
+  int N = 1000000;
+  cudaMalloc(&dev_send_buffer, N * sizeof(double));
+  cudaMalloc(&dev_recv_buffer, N * sizeof(double));
+
+  set_mem<<<512, 512>>>(dev_send_buffer, N, (double)rank);
+  cudaDeviceSynchronize();
+
+  // Send the buffer to the next rank
+  int dst = (rank + 1) % size;
+  int src = (rank - 1 + size) % size;
+  std::cout << "Sending to rank " << dst << ", receiving from rank " << src << std::endl;
+
+  for (int n = 0; n < 100; n++) {
+    size_t free_mem, total_mem;
+    MPI_Status status;
+    MPI_Sendrecv(dev_send_buffer, N, MPI_DOUBLE, dst, 0,
+                dev_recv_buffer, N, MPI_DOUBLE, src, 0,
+                MPI_COMM_WORLD, &status);
+    cudaMemGetInfo( &free_mem, &total_mem );
+    std::cout << "GPU memory: free=" << free_mem/1.0e9 << "GiB, total=" << total_mem/1.0e9 << "GiB" << std::endl;
+  }
+
+  std::vector<double> host_recv_buffer(N);
+  cudaMemcpy(host_recv_buffer.data(), dev_recv_buffer, N * sizeof(double), cudaMemcpyDeviceToHost);
+  std::cout << "value is " << host_recv_buffer[N - 1] << " on rank " << rank << std::endl;
+  
+  cudaFree(dev_send_buffer);
+  cudaFree(dev_recv_buffer);
+
+  MPI_Finalize();
+  
+  return 0;
 }

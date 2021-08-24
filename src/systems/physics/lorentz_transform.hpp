866133b0386e2ec9044e3fb90dab3d2cd4446290
fizban007 <fizban007@gmail.com>
2021-08-24 10:48:56 -0600

    added a 3D lorentz transformation function

---
+++ src/systems/physics/lorentz_transform.hpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2021 Alex Chen.
+ * This file is part of Aperture (https://github.com/fizban007/Aperture4.git).
+ *
+ * Aperture is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * Aperture is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _LORENTZ_TRANSFORM_H_
+#define _LORENTZ_TRANSFORM_H_
+
+#include "core/cuda_control.h"
+#include "core/math.hpp"
+#include "utils/util_functions.h"
+#include "utils/vec.hpp"
+
+namespace Aperture {
+
+template <typename Scalar>
+HD_INLINE vec_t<Scalar, 3>
+lorentz_transform(const vec_t<Scalar, 3>& u_orig, const vec_t<Scalar, 3>& v) {
+  Scalar gamma = 1.0f / math::sqrt(1.0f - v.dot(v));
+  Scalar udotv = u_orig.dot(v);
+
+  return (u_orig / gamma + v * (gamma * udotv / (gamma + 1.0f) - 1.0f)) *
+         (1.0f / (1.0f - udotv));
+}
+
+}  // namespace Aperture
+
+#endif  // _LORENTZ_TRANSFORM_H_

95c6dcd942884f54d63a5ef77d1f7f8c189d7511
fizban007 <fizban007@gmail.com>
2022-06-26 17:43:26 -0500

    put radiation test content into main polar cap setup

--- problems/polar_cap/src/main.cu
+++ problems/polar_cap/src/main.cu
@@ -18,7 +18,7 @@
 #include "data/rng_states.h"
 #include "framework/config.h"
 #include "framework/environment.h"
-// #include "systems/boundary_condition.h"
+#include "systems/boundary_condition.h"
 #include "systems/compute_lorentz_factor.h"
 #include "systems/data_exporter.h"
 #include "systems/domain_comm.h"
@@ -28,7 +28,8 @@
 #include "systems/policies/exec_policy_cuda.hpp"
 #include "systems/policies/coord_policy_cartesian.hpp"
 #include "systems/ptc_updater_base.h"
-#include "systems/radiation/curvature_emission_scheme_polar_cap.hpp"
+// #include "systems/radiation/curvature_emission_scheme_polar_cap.hpp"
+#include "systems/radiation/curvature_emission_scheme_gca_lite.hpp"
 #include "systems/radiative_transfer_impl.hpp"
 #include <iostream>
 
@@ -37,13 +38,12 @@ using namespace Aperture;
 
 namespace Aperture {
 
-template <typename Conf>
-void polar_cap_initial_condition(vector_field<Conf> &B0, particle_data_t &ptc,
-                                 rng_states_t &states);
-
 template class radiative_transfer<Config<3>, exec_policy_cuda,
                                   coord_policy_cartesian,
-                                  curvature_emission_scheme_polar_cap>;
+                                  // curvature_emission_scheme_polar_cap>;
+                                  curvature_emission_scheme_gca_lite>;
+
+template class ptc_updater_new<Config<3>, exec_policy_cuda, coord_policy_cartesian_gca_lite>;
 
 }  // namespace Aperture
 
@@ -51,39 +51,57 @@ int
 main(int argc, char *argv[]) {
   typedef Config<3> Conf;
   auto &env = sim_environment::instance(&argc, &argv);
-
-  env.params().add("log_level", (int64_t)LogLevel::info);
+  typedef typename Conf::value_t value_t;
 
   // auto comm = env.register_system<domain_comm<Conf>>(env);
   domain_comm<Conf> comm;
   grid_t<Conf> grid(comm);
 
-  auto pusher = env.register_system<
-      ptc_updater_new<Conf, exec_policy_cuda, coord_policy_cartesian>>(grid,
-                                                                       comm);
-  auto lorentz = env.register_system<compute_lorentz_factor_cu<Conf>>(grid);
-  auto momentum =
-      env.register_system<gather_momentum_space<Conf, exec_policy_cuda>>(grid);
   auto solver = env.register_system<field_solver_cu<Conf>>(grid, &comm);
-  // auto bc = env.register_system<boundary_condition<Conf>>(grid);
+  auto pusher = env.register_system<
+      ptc_updater_new<Conf, exec_policy_cuda, coord_policy_cartesian_gca_lite>>(grid);
+      // ptc_updater_new<Conf, exec_policy_cuda, coord_policy_cartesian>>(grid);
   auto rad = env.register_system<
       radiative_transfer<Conf, exec_policy_cuda, coord_policy_cartesian,
-                         curvature_emission_scheme_polar_cap>>(grid, &comm);
+                         curvature_emission_scheme_gca_lite>>(grid, &comm);
+                         // curvature_emission_scheme_polar_cap>>(grid, &comm);
+  auto lorentz = env.register_system<compute_lorentz_factor_cu<Conf>>(grid);
+  auto bc = env.register_system<boundary_condition<Conf>>(grid, &comm);
   auto exporter = env.register_system<data_exporter<Conf>>(grid, &comm);
 
   env.init();
 
   vector_field<Conf> *B0, *Bdelta, *Edelta;
+  value_t Bp = sim_env().params().get_as<double>("Bp", 1.0e3);
+  value_t Rpc = sim_env().params().get_as<double>("Rpc", 1.0);
+  value_t R_star = sim_env().params().get_as<double>("R_star", 10.0);
+
+  // Set initial dipole field
+  env.get_data("B0", &B0);
+  B0->set_values(0, [Bp, R_star](auto x, auto y, auto z) {
+    z = z / R_star + 1.0;
+    x /= R_star;
+    y /= R_star;
+    value_t r = math::sqrt(x * x + y * y + z * z);
+    return 3.0f * Bp * x * z / (r * r * r * r * r);
+  });
+  B0->set_values(1, [Bp, R_star](auto x, auto y, auto z) {
+    z = z / R_star + 1.0;
+    x /= R_star;
+    y /= R_star;
+    value_t r = math::sqrt(x * x + y * y + z * z);
+    return 3.0f * Bp * y * z / (r * r * r * r * r);
+  });
+  B0->set_values(2, [Bp, R_star](auto x, auto y, auto z) {
+    z = z / R_star + 1.0;
+    x /= R_star;
+    y /= R_star;
+    value_t r = math::sqrt(x * x + y * y + z * z);
+    return 3.0f * Bp * z * z / (r * r * r * r * r) - Bp / (r * r * r);
+  });
+
   particle_data_t *ptc;
   rng_states_t *states;
-
-  env.get_data("B0", &B0);
-  env.get_data("Bdelta", &Bdelta);
-  env.get_data("Edelta", &Edelta);
-  env.get_data("particles", &ptc);
-  env.get_data("rng_states", &states);
-
-  polar_cap_initial_condition(*B0, *ptc, *states);
 
   env.run();
   return 0;

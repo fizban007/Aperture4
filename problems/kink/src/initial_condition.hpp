3e9a934ad41630eb8172a2cd76fbd13f9c041217
fizban007 <fizban007@gmail.com>
2024-10-14 11:27:42 -0500

    added a kink initial condition

--- problems/kink/src/initial_condition.hpp
+++ problems/kink/src/initial_condition.hpp
@@ -14,7 +14,6 @@
 using namespace std;
 namespace Aperture {
 
-
 template <typename Conf>
 void
 kink_pressure_supported(vector_field<Conf> &B, particle_data_t &ptc,
@@ -201,11 +200,131 @@ kink_force_free(vector_field<Conf> &B, particle_data_t &ptc,
                      ptc.number());
 }
 
+template <typename Conf>
+void
+kink_pressure_supported_moving(vector_field<Conf> &B, vector_field<Conf> &E, double beta_z,
+                               particle_data_t &ptc, rng_states_t<exec_tags::device> &states) {
+  using value_t = typename Conf::value_t;
+
+  double gamma_z = 1.0 / std::sqrt(1.0 - beta_z * beta_z);
+  value_t B0 = sim_env().params().get_as<double>("B0", 100.0);
+  value_t sigma = sim_env().params().get_as<double>("sigma", 5.0);
+
+  value_t n_c = B0 * B0 / sigma;
+  value_t n_0 = sim_env().params().get_as<double>("n_0", n_c / 10.0);
+  value_t q_e = sim_env().params().get_as<double>("q_e", 1.0);
+  value_t ppc = sim_env().params().get_as<int64_t>("ppc", 4);
+
+  auto &grid = B.grid();
+  auto ext = grid.extent();
+
+  // Initialize the magnetic field values
+  auto Bphi = [B0] (auto r) {
+    return B0 * r * std::exp(1 - r);
+  };
+
+  B.set_values(0, [Bphi, gamma_z](auto x, auto y, auto z) {
+    auto r = math::sqrt(x*x + y*y);
+    return -gamma_z * y * Bphi(r) / r;
+  });
+  B.set_values(1, [Bphi, gamma_z](auto x, auto y, auto z) {
+    auto r = math::sqrt(x*x + y*y);
+    return gamma_z * x * Bphi(r) / r;
+  });
+
+  // TODO: check sign
+  E.set_values(0, [Bphi, beta_z, gamma_z](auto x, auto y, auto z) {
+    auto r = math::sqrt(x*x + y*y);
+    return gamma_z * beta_z * Bphi(r);
+  });
+
+  // profiles of physical quantities
+  auto jz_profile = [B0] LAMBDA (auto r) {
+    return B0 * (2.0 - r) * math::exp(1.0 - r);
+  };
+
+  auto n_profile = [n_c, n_0] LAMBDA (auto r) {
+    return n_0 + (n_c - n_0) / square(std::cosh(2.0 * r));
+  };
+
+  auto P_profile = [B0] LAMBDA (auto r) {
+    return square(B0) * (0.25 * math::exp(2.0 - 2.0 * r) * (1.0 + 2.0 * r - 2.0 * r * r) + math::exp(-2.0));
+  };
+
+  ptc_injector_dynamic<Conf> injector(grid);
+
+  // Jet particles
+  injector.inject_pairs(
+      // Injection criterion
+      [] LAMBDA(auto &pos, auto &grid, auto &ext) { return true; },
+      [ppc] LAMBDA(auto &pos, auto &grid, auto &ext) {return 2 * ppc; },
+      [jz_profile, n_profile, P_profile,
+       gamma_z, beta_z] LAMBDA(auto &x_global, rand_state &state, PtcType type) {
+        auto x = x_global[0];
+        auto y = x_global[1];
+        auto r = math::sqrt(x*x + y*y);
+
+        value_t n_p0 = n_profile(r);
+        value_t s_p0 = jz_profile(r) / 2.0;
+        value_t n_e0 = n_profile(r);
+        value_t s_e0 = -jz_profile(r) / 2.0;
+
+        value_t n_p = gamma_z * (n_p0 + s_p0 * beta_z);
+        value_t s_p = gamma_z * (s_p0 + n_p0 * beta_z);
+        value_t n_e = gamma_z * (n_e0 + s_e0 * beta_z);
+        value_t s_e = gamma_z * (s_e0 + n_e0 * beta_z);
+
+        // value_t kT = P_profile(r) / n_profile(r) / 2.0;
+        value_t kT_e = P_profile(r) / n_e / 2.0;
+        value_t kT_p = P_profile(r) / n_p / 2.0;
+
+        // value_t beta_d =  100.0 * std::exp(1.0 - std::sqrt(x * x + y * y)) * (2.0 - std::sqrt(x * x + y * y))
+        //                         / (n_0 + (n_c - n_0) / square(std::cosh(2.0 * std::sqrt(x * x + y * y))));
+        value_t beta_d = jz_profile(r) / n_profile(r) / 2.0;
+
+        vec_t<value_t, 3> u_e = rng_maxwell_juttner_drifting(state, kT_e, s_e / n_e);
+        vec_t<value_t, 3> u_p = rng_maxwell_juttner_drifting(state, kT_p, s_p / n_p);
+
+        if (type == PtcType::electron) {
+          return vec_t<value_t, 3>(u_e[1], u_e[2], u_e[0]);
+        } else {
+          return vec_t<value_t, 3>(u_p[1], u_p[2], u_p[0]);
+        }
+      },
+      // Particle weight
+      [jz_profile, n_profile, ppc, q_e, beta_z, gamma_z] LAMBDA(auto &x_global, PtcType type) {
+        auto x = x_global[0];
+        auto y = x_global[1];
+        auto r = math::sqrt(x*x + y*y);
+
+        value_t n_p0 = n_profile(r);
+        value_t s_p0 = jz_profile(r) / 2.0;
+        value_t n_e0 = n_profile(r);
+        value_t s_e0 = -jz_profile(r) / 2.0;
+
+        value_t n_p = gamma_z * (n_p0 + s_p0 * beta_z);
+        value_t n_e = gamma_z * (n_e0 + s_e0 * beta_z);
+        if (type == PtcType::electron) {
+          return n_e / ppc / q_e;
+        } else {
+          return n_p / ppc / q_e;
+        }
+      });
+
+  Logger::print_info("After initial condition, there are {} particles",
+                     ptc.number());
+}
+
 template void kink_pressure_supported<Config<3>>(vector_field<Config<3>> &B,
                                                 particle_data_t &ptc,
                                                 rng_states_t<exec_tags::device> &states);
 
 template void kink_force_free<Config<3>>(vector_field<Config<3>> &B, particle_data_t &ptc,
                               rng_states_t<exec_tags::device> &states);
+
+template void kink_pressure_supported_moving(vector_field<Config<3>> &B, vector_field<Config<3>> &E,
+                                             double beta_z, particle_data_t &ptc,
+                                             rng_states_t<exec_tags::device> &states);
+
 
 }  // namespace Aperture

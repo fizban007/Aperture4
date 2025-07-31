18e791df94ee4460875dc7ce75e7f86dee1c7a1d
NRackers <n.rackers@wustl.edu>
2025-07-31 12:51:42 -0500

    latest_gca_test

--- problems/magnetar/src/single_ptc_gca.cpp
+++ problems/magnetar/src/single_ptc_gca.cpp
@@ -15,105 +15,175 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "core/math.hpp"
-#include "framework/config.h"
-#include "framework/environment.h"
-#include "systems/compute_moments.h"
-#include "systems/data_exporter.h"
-#include "systems/field_solver_sph.h"
-#include "systems/gather_tracked_ptc.h"
-#include "systems/grid_sph.hpp"
-#include "systems/policies/coord_policy_spherical_gca.hpp"
-#include "systems/policies/exec_policy_dynamic.hpp"
-#include "systems/ptc_injector_new.h"
-#include "systems/ptc_updater_impl.hpp"
-#include "systems/radiative_transfer_impl.hpp"
-#include "systems/resonant_scattering_scheme_gca.hpp"
-// #include "systems/boundary_condition.h"
-#include <iostream>
-
-namespace Aperture {
-
-template class radiative_transfer<Config<2>, exec_policy_dynamic,
-                                  coord_policy_spherical_gca,
-                                  resonant_scattering_scheme>;
-
-}
-
-using namespace std;
-using namespace Aperture;
-
-int
-main(int argc, char *argv[]) {
-  typedef Config<2> Conf;
-  using value_t = typename Config<2>::value_t;
-  auto &env = sim_environment::instance(&argc, &argv);
-
-  domain_comm<Conf, exec_policy_dynamic> comm;
-  grid_sph_t<Conf> grid(comm);
-  auto pusher = env.register_system<
-      ptc_updater<Conf, exec_policy_dynamic, coord_policy_spherical_gca>>(
-      grid, &comm);
-  auto tracker =
-      env.register_system<gather_tracked_ptc<Conf, exec_policy_dynamic>>(grid);
-  // auto rad = env.register_system<radiative_transfer<
-  //     Conf, exec_policy_dynamic, coord_policy_spherical,
-  //     resonant_scattering_scheme>>( grid, &comm);
-  auto exporter = env.register_system<data_exporter<Conf, exec_policy_dynamic>>(
-      grid, &comm);
-
-  env.init();
-
-  double Bp = 10000.0;
-  double BQ = 1000.0;
-  double ptc_rmax = 8.0;
-  double ptc_r_init = 1.1;
-  env.params().get_value("Bp", Bp);
-  env.params().get_value("B_Q", BQ);
-  env.params().get_value("ptc_rmax", ptc_rmax);
-  env.params().get_value("ptc_r_init", ptc_r_init);
-
-  // Set initial condition
-  // set_initial_condition(env, *grid, 0, 1.0, Bp);
-  vector_field<Conf> *B0, *B;
-  particle_data_t *ptc;
-  // env.get_data("B0", &B0);
-  env.get_data("B", &B);
-  env.get_data("particles", &ptc);
-
-  // Set dipole initial magnetic field
-  B->set_values(0, [Bp](Scalar x, Scalar theta, Scalar phi) {
-    Scalar r = grid_sph_t<Conf>::radius(x);
-    // return Bp / (r * r);
-    return Bp * 2.0 * cos(theta) / cube(r);
-  });
-  B->set_values(1, [Bp](Scalar x, Scalar theta, Scalar phi) {
-    Scalar r = grid_sph_t<Conf>::radius(x);
-    return Bp * sin(theta) / cube(r);
-  });
-
-  // Add a single particle to the magnetosphere
-  // Scalar p0 = 1000.0f;
-  // Scalar r0 = 1.1f;
-  // Scalar th0 = 0.338f;
-  value_t th0 = math::asin(math::sqrt(ptc_r_init / ptc_rmax));
-  value_t r0 = ptc_r_init;
-
-  value_t gamma =
-      100.0 * Bp / BQ / cube(r0) * math::sqrt(1.0 + 3.0 * cos(th0) * cos(th0));
-  value_t p0 = math::sqrt(gamma * gamma - 1.0);           // gamma = sqrt(1+p^2)
-  p0 = p0 / math::sqrt(1.0 + 3.0 * cos(th0) * cos(th0));  // Normalization
-
-  int N = 5000;
-  for (int i = 0; i < N; i++) {
-    // ptc->append(exec_tags::device{}, {0.5f, 0.5f, 0.0f}, {p0, 0.0f, 0.0f}, 10
-    // + 60 * grid->dims[0],
-    //                 100.0);
-    ptc_append_global(exec_policy_dynamic<Conf>::exec_tag{}, *ptc, grid,
-                      {grid_sph_t<Conf>::from_radius(r0), th0, 0.0f},
-                      {p0, 0.0f, 0.0f}, 1.0f, flag_or(PtcFlag::tracked));
-  }
-
-  env.run();
-  return 0;
-}
+ #include "core/math.hpp"
+ #include "framework/config.h"
+ #include "framework/environment.h"
+ #include "systems/compute_moments.h"
+ #include "systems/data_exporter.h"
+ #include "systems/field_solver_sph.h"
+ #include "systems/gather_tracked_ptc.h"
+ #include "systems/grid_sph.hpp"
+ #include "systems/policies/coord_policy_spherical_gca.hpp"
+ #include "systems/policies/exec_policy_dynamic.hpp"
+ #include "systems/ptc_injector_new.h"
+ #include "systems/ptc_updater_impl.hpp"
+ #include "systems/radiative_transfer_impl.hpp"
+ #include "systems/resonant_scattering_scheme_gca.hpp"
+ // #include "systems/boundary_condition.h"
+ #include <iostream>
+ 
+ namespace Aperture {
+ 
+ template class radiative_transfer<Config<2>, exec_policy_dynamic,
+                                   coord_policy_spherical_gca,
+                                   resonant_scattering_scheme>;
+ 
+ }
+ 
+ using namespace std;
+ using namespace Aperture;
+ 
+ int
+ main(int argc, char *argv[]) {
+   typedef Config<2> Conf;
+   using value_t = typename Config<2>::value_t;
+   auto &env = sim_environment::instance(&argc, &argv);
+ 
+   domain_comm<Conf, exec_policy_dynamic> comm;
+   grid_sph_t<Conf> grid(comm);
+   auto pusher = env.register_system<
+       ptc_updater<Conf, exec_policy_dynamic, coord_policy_spherical_gca>>(
+       grid, &comm);
+   auto tracker =
+       env.register_system<gather_tracked_ptc<Conf, exec_policy_dynamic>>(grid);
+   // auto rad = env.register_system<radiative_transfer<
+   //     Conf, exec_policy_dynamic, coord_policy_spherical_gca,
+   //     resonant_scattering_scheme>>( grid, &comm);
+   auto exporter = env.register_system<data_exporter<Conf, exec_policy_dynamic>>(
+       grid, &comm);
+ 
+   env.init();
+ 
+   double Bp = 10000.0;
+   double BQ = 1000.0;
+   double ptc_rmax = 8.0;
+   double ptc_r_init = 1.1;
+   double ptc_gamma = 100.0;
+   int ppc = 10;
+   env.params().get_value("Bp", Bp);
+   env.params().get_value("B_Q", BQ);
+   env.params().get_value("ptc_rmax", ptc_rmax);
+   env.params().get_value("ptc_r_init", ptc_r_init);
+   env.params().get_value("ptc_gamma", ptc_gamma);
+   env.params().get_value("ppc", ppc);
+   
+ 
+   // Set initial condition
+   // set_initial_condition(env, *grid, 0, 1.0, Bp);
+   vector_field<Conf>  *B,*E;
+   particle_data_t *ptc;
+   // env.get_data("B0", &B0);
+   env.get_data("B", &B);
+   env.get_data("E", &E);
+   double Ep = 500.0;
+   env.get_data("particles", &ptc);
+ 
+   // Set dipole initial magnetic field
+   B->set_values(0, [Bp](Scalar x, Scalar theta, Scalar phi) {
+     Scalar r = grid_sph_t<Conf>::radius(x);
+     // return Bp / (r * r);
+     return Bp * 2.0 * cos(theta) / cube(r);
+   });
+   B->set_values(1, [Bp](Scalar x, Scalar theta, Scalar phi) {
+     Scalar r = grid_sph_t<Conf>::radius(x);
+     return Bp * sin(theta) / cube(r);
+   });
+   E->set_values(0, [Ep](Scalar x, Scalar theta, Scalar phi) {
+     Scalar r = grid_sph_t<Conf>::radius(x);
+     // return Bp / (r * r);
+     return -Ep * 2.0 * cos(theta) / cube(r);
+   });
+   E->set_values(1, [Ep](Scalar x, Scalar theta, Scalar phi) {
+     Scalar r = grid_sph_t<Conf>::radius(x);
+     return -Ep * sin(theta) / cube(r);
+   });
+ 
+   // Add a single particle to the magnetosphere
+   // Scalar p0 = 1000.0f;
+   // Scalar r0 = 1.1f;
+   // Scalar th0 = 0.338f;
+   // for (value_t r_max = 2.0; r_max <= ptc_rmax; r_max += 1.0) {
+   //   // ptc->append(exec_tags::device{}, {0.5f, 0.5f, 0.0f}, {p0, 0.0f, 0.0f},
+   //   //             10 + 60 * grid->dims[0], 100.0);
+     
+   //   value_t th0 = math::asin(math::sqrt(ptc_r_init / r_max));
+   //   value_t r0 = ptc_r_init;
+ 
+   //   value_t gamma = ptc_gamma;
+   //   //     100.0 * Bp / BQ / cube(r0) * math::sqrt(1.0 + 3.0 * cos(th0) * cos(th0));
+   //   value_t p0 = math::sqrt(gamma * gamma - 1.0);           // gamma = sqrt(1+p^2)
+   //   p0 = p0 / math::sqrt(1.0 + 3.0 * cos(th0) * cos(th0));  // Normalization
+ 
+   //   int N = 1;
+   //   for (int i = 0; i < N; i++) {
+   //     // ptc->append(exec_tags::device{}, {0.5f, 0.5f, 0.0f}, {p0, 0.0f, 0.0f}, 10
+   //     // + 60 * grid->dims[0],
+   //     //                 100.0);
+   //     ptc_append_global(exec_policy_dynamic<Conf>::exec_tag{}, *ptc, grid,
+   //                       {grid_sph_t<Conf>::from_radius(r0), th0, 0.0f},
+   //                       {p0, 0.0f, 0.0f}, 1.0f, flag_or(PtcFlag::tracked));
+   //   }
+   // }
+   ptc_injector_dynamic<Conf> injector(grid);
+   injector.inject_pairs(
+     // Injection criterion (in flux tube)
+       [] LAMBDA(auto &pos, auto &grid, auto &ext) { 
+         // return true; // Inject everywhere
+         //TODO check if this is the actual r we are trying to input
+         auto r = math::exp(grid.template coord<0>(pos, 0.5f));// Since r_grid = log(r)
+         auto th = grid.template coord<1>(pos, 0.5f);
+         auto r_max_check = r / (sin(th) * sin(th));
+         if (r>1.1 && r < 1.2 && th < M_PI / 2.0-0.2) {
+             //printf("r_max_check is %f\n tube_rmax_1/2 is %f,%f", r_max_check, tube_rmax_1, tube_rmax_2);
+           return true;
+         }else {
+           return false;
+         }
+         },
+       // Number injected per cell
+       [ppc] LAMBDA(auto &pos, auto &grid, auto &ext) { return ppc; },
+         // Initialize particle momentum
+       [Bp,BQ] LAMBDA(auto &x_global, rand_state &state, PtcType type) {
+         auto &grid = static_cast<const grid_sph_t<Conf> &>(
+             exec_policy_dynamic<Conf>::grid());
+         auto r =grid.radius(x_global[0]);
+         auto th = grid.theta(x_global[1]);
+         // From Belobodorov 2013 gamma is 100b where b = B/BQ
+         //TODO check if this is the correct conversion to momentum
+         //value_t gamma = 100*math::sqrt(Bp/ cube(r) * (4*cos(th)*cos(th) + sin(th)*sin(th)))/BQ; // i.e Sqrt(B1^2+B2^2)/BQ
+         value_t gamma = 100 * Bp/BQ / cube(r) * math::sqrt( 1 + 3*cos(th)*cos(th));
+         gamma = 1000;
+         value_t p0 = math::sqrt(gamma*gamma - 1.0); // gamma = sqrt(1+p^2)
+         p0 = p0 / math::sqrt( 1.0 + 3.0 * cos(th)*cos(th) ); // Normalization
+         if (th <= M_PI / 2.0) {
+           // return vec_t<value_t, 3>(p0 * 2.0 * cos(th), p0 * sin(th), 0);
+           return vec_t<value_t, 3>(p0, 0, 0); // Inject at the gamma for p_para
+         } else {
+           // return vec_t<value_t, 3>(-p0 * 2.0 * cos(th), -p0 * sin(th), 0);
+           return vec_t<value_t, 3>(-p0, 0, 0); // Inject at the gamma for p_para
+         }
+         //return vec_t<value_t, 3>(p0 * 2.0 * cos(th), p0 * sin(th), 0);
+       },
+         // Initialize particle weight (i.e makes each particle worth a different amount of "actual" particles)
+       [] LAMBDA(auto &x_global, PtcType type) {
+         // auto &grid = static_cast<const grid_sph_t<Conf> &>(
+         //     exec_policy_dynamic<Conf>::grid());
+         //auto r = grid_sph_t<Conf>::radius(x_global[0]);
+         auto th = grid_sph_t<Conf>::theta(x_global[1]);
+         return 1.0 * math::sin(th);
+       }
+       );
+   env.run();
+   return 0;
+ }
+ 
